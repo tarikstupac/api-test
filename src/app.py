80dfe8fed102d65949730ab16fb3bf52f8445d93
Tarik Stupac <tarik.stupac@zendev.se>
2021-04-08 14:15:53 +0200

    Added an endpoint to get user by id

--- src/app.py
+++ src/app.py
@@ -1,10 +1,11 @@
 import falcon
 import json
-from src.resources.user_resource import UsersResource
+from src.resources.user_resource import UsersResource, UserResource
 
 
 api = falcon.App()
-api.add_route('/users/', UsersResource())
+api.add_route('/users', UsersResource())
+api.add_route('/users/{user_id}', UserResource())
 
 
 if __name__ == "__main__":
--- src/resources/user_resource.py
+++ src/resources/user_resource.py
@@ -14,10 +14,34 @@ db_session = Session()
 class UsersResource(object):
 
     def on_get(self, req, resp):
-        resp.status = falcon.HTTP_200
         users = db_session.query(User).all()
-        result = users_schema.dumps(users)
-        resp.body = result
+        if users is not None:
+            result = users_schema.dumps(users)
+            resp.status = falcon.HTTP_200
+            resp.body = result
+        else:
+            resp.status = falcon.HTTP_404
+
+
+class UserResource(object):
+
+    def on_get(self, req, resp, user_id):
+        if int(user_id) == 0:
+            resp.status = falcon.HTTP_400
+            resp.body = json.dumps({"message":"User id can not be 0"})
+        else:
+            user = db_session.query(User).filter(User.id == user_id).first()
+            if user is None:
+                resp.status = falcon.HTTP_404
+                resp.body = json.dumps({"message":"User with that id does not exist"})
+            else:
+                result = user_schema.dumps(user)
+                resp.status = falcon.HTTP_200
+                resp.body = result
+                     
+
+
+    
 
 
         
